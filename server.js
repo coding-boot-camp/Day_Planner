005e716b6f8b04dc079e047462fd53db08dd845e
Orlando Buzana <orlandobuzana@outlook.com>
2019-09-16 23:03:53 -0400

    added some styling to /views/index.handlebars. added lines 11,12,13,14, to use Heroku env vaviables

---
+++ server.js
@@ -0,0 +1,99 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+// process.env SQL variables set on Heroku
+var PORT = process.env.PORT || 8080;
+var SQL_DB = process.env.SQL_DB;
+var SQL_PASS = process.env.SQL_PASS;
+var SQL_USER = process.env.SQL_USER;
+var SQL_HOST = process.env.SQL_HOST;
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: SQL_HOST,
+  port: 3306,
+  user: SQL_USER,
+  password: SQL_PASS,
+  database: SQL_DB
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Use Handlebars to render the main index.html page with the plans in it.
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM plans;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.render("index", { plans: data });
+  });
+});
+
+// Create a new plan
+app.post("/api/plans", function(req, res) {
+  connection.query("INSERT INTO plans (plan) VALUES (?)", [req.body.plan], function(err, result) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    // Send back the ID of the new plan
+    res.json({ id: result.insertId });
+    console.log({ id: result.insertId });
+  });
+});
+
+// Update a plan
+app.put("/api/plans/:id", function(req, res) {
+  connection.query("UPDATE plans SET plan = ? WHERE id = ?", [req.body.plan, req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.changedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
+// Delete a plan
+app.delete("/api/plans/:id", function(req, res) {
+  connection.query("DELETE FROM plans WHERE id = ?", [req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
